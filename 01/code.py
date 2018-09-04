3a619e955e3f29eeb3ce74dc384f285ceac86d01
shaniphankar <shaniphankar@gmail.com>
2018-09-04 08:35:52 +0530

    cleand up the sql code

--- 01/code.py
+++ 01/code.py
@@ -1,17 +1,20 @@
 import sqlite3
 import numpy as np
 
+def exec(conn,query):
+	c=conn.cursor()
+	c.execute(query)
+	res=c.fetchall()
+	c.close()
+	return res
 
 def main():
 	conn=sqlite3.connect('01/database.sqlite')
-	c=conn.cursor()
 	query="""select name from sqlite_master where type = 'table';"""
-	c.execute(query)
-	print(c.fetchall())
+	print(exec(conn,query))
 	#It contatins 1 table called Reviews
 	query="""select * from Reviews limit 5"""
-	c.execute(query)
-	print(c.fetchall())
+	print(exec(conn,query))
 	
 
 if __name__ == '__main__':
