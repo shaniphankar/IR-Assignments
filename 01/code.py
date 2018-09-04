7a738b4cfac2057b042b552d14917aeb5d9a5e8e
shaniphankar <shaniphankar@users.noreply.github.com>
2018-09-04 17:15:09 +0530

    Add files via upload

--- 01/code.py
+++ 01/code.py
@@ -8,9 +8,20 @@ import pprint
 def main():
 	with(open('./yelp-dataset/yelp_academic_dataset_review.json')) as f:
 		objects =(json.loads(line) for line in f)
-		objects = islice(objects,3)
-		pp=pprint.PrettyPrinter(indent=4)
-		pp.pprint(list(objects))
+		print(objects)
+		reviews=[{}]
+		i=0
+		for x in objects:
+			pp=pprint.PrettyPrinter(indent=4)
+			print(nltk.word_tokenize(x['text']))
+			#reviews.append({'business':x['business_id'],'stars':x['stars'],'text':x['text']})
+			#print(reviews[i])
+			i+=1
+			
+			#pp.pprint(reviews)
+
+
+		
 
 if __name__ == '__main__':
 	main()
